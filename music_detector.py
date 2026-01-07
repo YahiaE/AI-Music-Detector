dd55d7eb31f88228f9f442e3527e979ccc4d3ef8
YahiaE <yahiae2002@gmail.com>
2026-01-07 02:07:15 -0500

    Extract spectral centroids from AI Audio + Correct comments on onset strength

--- music_detector.py
+++ music_detector.py
@@ -14,9 +14,39 @@ import os.path
 for i in range(1,6): # go through the 5 files and load them through librosa => returns audio time series + sampling rate
     # audio time series = a sequence of data points representing sound, plotted as amplitude (loudness) against time
     y, sr = librosa.load(os.path.join(f'data\\ai\\ai_audio_{i}.wav'))
-    onset_env = librosa.onset.onset_strength(y=y, sr=sr) # detects how strong of a change of frequency has occurred throughout the sound
-    # tempo measures how frequent sudden changes of frequency happen, which is why onset_strength is needed
+    '''
+    Onset Strength => How much a sound has changed compared to the moment before
+    - Characteristics used to measure the change = Frequency, Amplitude, Tone (texture of the sound)
+    '''
+    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+    # tempo measures how often a pattern of changes in the sound occur over time, which is why onset_strength is needed
     t = librosa.feature.tempo(onset_envelope=onset_env,sr=sr) 
-    print("\nAmp over time:", y, "\nSampling Rate:", sr, "\nTempo:", t)
+    
+    '''
+    Spectral Centroids: Shows which frequencies have the highest amplitudes
+    - Low frequencies have the highest amplitudes => Sound is rather dark / bassy
+    - High frequencies have the highest amplitudes => Sound is bright / sharp
+     
+    Uses the weighted sum of all frequencies multiplied by their amplitudes divided by the total amplitude
+    - Result is measured in Hertz (Hz) (Lower => Darker, Higher => Brighter)
+
+    | Frequency (Hz) | Amplitude |
+    | -------------- | --------- |
+    | 100            | 1         |
+    | 500            | 2         |
+    | 1000           | 5         | => High freq, high amp
+
+    | Frequency (Hz) | Amplitude |
+    | -------------- | --------- |
+    | 100            | 5         | => Low freq, high amp
+    | 500            | 2         |
+    | 1000           | 1         | 
+    '''
+    s_cent = librosa.feature.spectral_centroid(y=y, sr=sr) 
+    print("\nAmp over time:", y, "\nSampling Rate:", sr, "\nTempo:", t, "\nSpectral Centroid:", s_cent)
+
+   
+ 
+
 
 
