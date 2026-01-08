a230995389df400061bf4540814b89a837a533c2
Yahia Elhag <yahiae2002@gmail.com>
2026-01-08 01:13:20 -0500

    Extract MFCCs from AI Audios + Provide brief definition about MFCCs that will be expanded on tomorrow

--- music_detector.py
+++ music_detector.py
@@ -1,5 +1,5 @@
-'''
-Libraries: 
+"""
+Libraries:
 - librosa: audio/music analysis (converts audio to numbers)
 - numpy: handles arrays of numbers from music (music can contains millions of data points, numpy utilizes arrays that are much faster than
 py arrays)
@@ -7,22 +7,25 @@ py arrays)
 - scikit-learn: used to train models
 - matplotlib: plot graphs to show accuracy + used for debugging
 - jupyter: splits code into runable individual chunks (good for learning / debugging)
-'''
+"""
+
 import librosa
 import os.path
 
-for i in range(1,6): # go through the 5 files and load them through librosa => returns audio time series + sampling rate
+for i in range(
+    1, 6
+):  # go through the 5 files and load them through librosa => returns audio time series + sampling rate
     # audio time series = a sequence of data points representing sound, plotted as amplitude (loudness) against time
-    y, sr = librosa.load(os.path.join(f'data\\ai\\ai_audio_{i}.wav'))
-    '''
+    y, sr = librosa.load(os.path.join(f"data\\ai\\ai_audio_{i}.wav"))
+    """
     Onset Strength => How much a sound has changed compared to the moment before
     - Characteristics used to measure the change = Frequency, Amplitude, Tone (texture of the sound)
-    '''
+    """
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
     # tempo measures how often a pattern of changes in the sound occur over time, which is why onset_strength is needed
-    t = librosa.feature.tempo(onset_envelope=onset_env,sr=sr) 
-    
-    '''
+    t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
+
+    """
     Spectral Centroids: Shows which frequencies have the highest amplitudes
     - Low frequencies have the highest amplitudes => Sound is rather dark / bassy
     - High frequencies have the highest amplitudes => Sound is bright / sharp
@@ -41,12 +44,24 @@ for i in range(1,6): # go through the 5 files and load them through librosa => r
     | 100            | 5         | => Low freq, high amp
     | 500            | 2         |
     | 1000           | 1         | 
-    '''
-    s_cent = librosa.feature.spectral_centroid(y=y, sr=sr) 
-    print("\nAmp over time:", y, "\nSampling Rate:", sr, "\nTempo:", t, "\nSpectral Centroid:", s_cent)
+    """
+    s_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
 
-   
- 
+    """
+    [Expand on this definition / meaning]
+    MFCC (Mel-Frequency Cepstral Coefficients): Describes the timbre / shape of a sound. 
+    """
+    mfccs = librosa.feature.mfcc(y=y, sr=sr)
 
-
-
+    print(
+        "\nAmp over time:",
+        y,
+        "\nSampling Rate:",
+        sr,
+        "\nTempo:",
+        t,
+        "\nSpectral Centroid:",
+        s_cent,
+        "\nMFCC:",
+        mfccs,
+    )
