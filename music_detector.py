d13d3b6ce4477face9023788526b188568432bee
Yahia Elhag <yahiae2002@gmail.com>
2026-01-11 01:07:18 -0500

    Calculate and print average tempo, average amplitude over time, MFCCS shapes, and average spectral centroid for AI Audios

--- music_detector.py
+++ music_detector.py
@@ -11,19 +11,29 @@ py arrays)
 
 import librosa
 import os.path
+import numpy as np
+
+total_tempo = 0
+total_spectral_centroids = 0
+total_ats = 0
+mfccs_shapes = []
+
 
 for i in range(
     1, 6
 ):  # go through the 5 files and load them through librosa => returns audio time series + sampling rate
     # audio time series = a sequence of data points representing sound, plotted as amplitude (loudness) against time
     y, sr = librosa.load(os.path.join(f"data\\ai\\ai_audio_{i}.wav"))
+    total_ats += y
     """
     Onset Strength => How much a sound has changed compared to the moment before
     - Characteristics used to measure the change = Frequency, Amplitude, Tone (texture of the sound)
     """
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
     # tempo measures how often a pattern of changes in the sound occur over time, which is why onset_strength is needed
-    t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
+    t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)[0]
+
+    total_tempo += t
 
     """
     Spectral Centroids: Shows which frequencies have the highest amplitudes
@@ -46,43 +56,46 @@ for i in range(
     | 1000           | 1         | 
     """
     s_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+    total_spectral_centroids += s_cent
 
     """
     [Expand on this definition / meaning]
     MFCC (Mel-Frequency Cepstral Coefficients): Describes the timbre / shape of a sound. 
     """
     mfccs = librosa.feature.mfcc(y=y, sr=sr)
+    mfccs_shapes.append(mfccs.shape)
 
-    print(
-        "\nAmp over time:",
-        y,
-        "\nSampling Rate:",
-        sr,
-        "\nTempo:",
-        t,
-        "\nSpectral Centroid:",
-        s_cent,
-        "\nMFCC:",
-        mfccs,
-    )
+print(
+    "\nAvergage amp over time:",
+    (np.sum(total_ats) / total_ats.size),
+    "\nSampling Rate:",
+    sr,
+    "\nTempo:",
+    total_tempo / 6,
+    "\nSpectral Centroid:",
+    (np.sum(total_spectral_centroids) / total_spectral_centroids.size),
+    "\nMFCC:",
+    mfccs_shapes,
+)
 
-for i in range(1, 6):
-    y, sr = librosa.load(os.path.join(f"data\\human\\human_audio_{i}.wav"))
 
-    onset_env = librosa.onset.onset_strength(y=y, sr=sr)
-    t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
-    s_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr)
+# for i in range(1, 6):
+#     y, sr = librosa.load(os.path.join(f"data\\human\\human_audio_{i}.wav"))
 
-    print(
-        "\nAmp over time:",
-        y,
-        "\nSampling Rate:",
-        sr,
-        "\nTempo:",
-        t,
-        "\nSpectral Centroid:",
-        s_cent,
-        "\nMFCC:",
-        mfccs,
-    )
+#     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
+#     t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)
+#     s_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+#     mfccs = librosa.feature.mfcc(y=y, sr=sr)
+
+#     print(
+#         "\nAmp over time:",
+#         y,
+#         "\nSampling Rate:",
+#         sr,
+#         "\nTempo:",
+#         t,
+#         "\nSpectral Centroid:",
+#         s_cent,
+#         "\nMFCC:",
+#         mfccs,
+#     )
