4a0bf83cbd6f0c8a1bcbc24843563f64ae1ff57c
Yahia Elhag <yahiae2002@gmail.com>
2026-01-11 14:27:23 -0500

    Graph and display the average absolute amplitude over time + rename variables to be more accurate

--- music_detector.py
+++ music_detector.py
@@ -12,11 +12,13 @@ py arrays)
 import librosa
 import os.path
 import numpy as np
+import matplotlib.pyplot as plt
 
-total_tempo = 0
-total_spectral_centroids = 0
-total_ats = 0
-mfccs_shapes = []
+total_tempo_ai = 0
+total_spectral_centroids_ai = 0
+total_ats_ai = 0
+mfccs_shapes_ai = []
+sample_rate = 0
 
 
 for i in range(
@@ -24,7 +26,8 @@ for i in range(
 ):  # go through the 5 files and load them through librosa => returns audio time series + sampling rate
     # audio time series = a sequence of data points representing sound, plotted as amplitude (loudness) against time
     y, sr = librosa.load(os.path.join(f"data\\ai\\ai_audio_{i}.wav"))
-    total_ats += y
+    sample_rate = sr
+    total_ats_ai += y
     """
     Onset Strength => How much a sound has changed compared to the moment before
     - Characteristics used to measure the change = Frequency, Amplitude, Tone (texture of the sound)
@@ -33,7 +36,7 @@ for i in range(
     # tempo measures how often a pattern of changes in the sound occur over time, which is why onset_strength is needed
     t = librosa.feature.tempo(onset_envelope=onset_env, sr=sr)[0]
 
-    total_tempo += t
+    total_tempo_ai += t
 
     """
     Spectral Centroids: Shows which frequencies have the highest amplitudes
@@ -56,27 +59,43 @@ for i in range(
     | 1000           | 1         | 
     """
     s_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
-    total_spectral_centroids += s_cent
+    total_spectral_centroids_ai += s_cent
 
     """
     [Expand on this definition / meaning]
     MFCC (Mel-Frequency Cepstral Coefficients): Describes the timbre / shape of a sound. 
     """
     mfccs = librosa.feature.mfcc(y=y, sr=sr)
-    mfccs_shapes.append(mfccs.shape)
+    mfccs_shapes_ai.append(mfccs.shape)
 
 print(
     "\nAvergage amp over time:",
-    (np.sum(total_ats) / total_ats.size),
+    (np.sum(total_ats_ai) / total_ats_ai.size),
     "\nSampling Rate:",
     sr,
     "\nTempo:",
-    total_tempo / 6,
+    total_tempo_ai / 6,
     "\nSpectral Centroid:",
-    (np.sum(total_spectral_centroids) / total_spectral_centroids.size),
+    (np.sum(total_spectral_centroids_ai) / total_spectral_centroids_ai.size),
     "\nMFCC:",
-    mfccs_shapes,
+    mfccs_shapes_ai,
 )
+
+print(total_ats_ai.size)
+fig, ax = plt.subplots()
+# Sample = Measurement of amplitude at a very specific point in time. The # of samples is dependent on the sampling rate (# of samples taken per second)
+time = (
+    total_ats_ai.size // sample_rate
+)  # Seconds = total # of samples / sample_rate (# of samples per second) => equals to 30 seconds
+
+# Plots absol
+ax.plot(
+    np.arange(time),  # X-Axis
+    [
+        np.mean(np.abs(total_ats_ai[i * sr : (i + 1) * sr])) for i in range(time)
+    ],  # Y-Axis (average of absolute amplitude per sample group (dependent on sample rate)). Absolute amplitude is more accurate to represent loudness + Positive/Negative amplitudes can cancel out, which can make the graph misleading
+)
+plt.show()
 
 
 # for i in range(1, 6):
